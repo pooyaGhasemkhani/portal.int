96c58a9c4056933f0804f8315bd167a95c1d07ae
pooyauser.email <pooyaghasemkhani@gmail.com>
2023-10-16 11:24:46 +0330

    fix: some edit

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,55 +2,57 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const express = require('express'); 
+module.exports = (env)=> {
 
-module.exports = {
-    entry: './src/main.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath:'/'
-    },
-    mode: 'development',
-    devServer: {
-        port: 8080,
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['css-loader'],
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader', 'css-loader', 'sass-loader'
-                ]
-            },
-            {
-                test: /\.hbs$/,
-                loader: 'handlebars-loader'
-              },
-              {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
+     return {
+        entry: './src/main.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, './dist'),
+            publicPath:'/'
+        },
+        mode: 'development',
+        devServer: {
+            port: 8080,
+            historyApiFallback: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['css-loader'],
                 },
-              },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'portal',
-            description: 'your description here',
-            template: './src/index.html'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: './src/constants/jsons', to: './constants/jsons' },
-                { from: './src/assets', to: 'assets' },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'style-loader', 'css-loader', 'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.hbs$/,
+                    loader: 'handlebars-loader'
+                  },
+                  {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                      loader: 'babel-loader',
+                    },
+                  },
             ],
-        }),
-    ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'portal',
+                description: 'your description here',
+                template: './src/index.html'
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: './src/constants/jsons', to: './constants/jsons' },
+                    { from: './src/assets', to: 'assets' },
+                ],
+            }),
+        ],
+     }
 };
